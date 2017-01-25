bfb56f611432eab8c4af6500a4bdeffa132d7d1c
Michael Jones <mdeanjones@gmail.com>
2017-01-25 15:49:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-var */
+
+module.exports = {
+  name: 'ember-cli-svn',
+
+
+  dependencies() {
+    const dependencies = {};
+    const svnDependencies = this.parent.pkg.svnDependencies;
+
+    if (svnDependencies) {
+      Object.keys(svnDependencies).forEach((key) => {
+        const tagIdx = key.indexOf('@');
+        const value = svnDependencies[key];
+        key = tagIdx === -1 ? key : key.substring(0, tagIdx);
+
+        dependencies[key] = value;
+      });
+    }
+
+    this.pkg = this.pkg || {};
+    this.pkg.dependencies = dependencies;
+
+    return dependencies;
+  },
+};
