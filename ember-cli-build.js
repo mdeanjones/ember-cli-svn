a9ebd1f71242b880b7280c5ddfe11551cdf748e5
Michael Jones <mdeanjones@gmail.com>
2017-01-25 17:34:51 -0500

    Now with testing... scaffolding.

---
+++ ember-cli-build.js
@@ -0,0 +1,7 @@
+/* global require, module */
+const EmberApp = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  const app = new EmberApp(defaults, {});
+  return app.toTree();
+};
